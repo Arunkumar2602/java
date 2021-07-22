59e0841d1ae41e33e5c808ff0b5d3af7b2b1f91e
Arunkumar2602 <arunrockz01@gmail.com>
2021-07-22 13:13:36 -0400

    hari push

--- src/org/sample/Java.java
+++ src/org/sample/Java.java
@@ -2,7 +2,9 @@ package org.sample;
 
 public class Java {
        
-	
+	public void password() {
+       System.out.println("password");		
+}
 	public static void main(String[] args) {
 	    String s = "Hello world";
 	    System.out.println(s);
